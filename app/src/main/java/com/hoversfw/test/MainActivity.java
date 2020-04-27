f25d99e44391d84932d57e52f1c8a9fffc5638a4
Your Name <liubaoren2006@gmail.com>
2020-04-27 17:52:41 -0400

    test

--- app/src/main/java/com/hoversfw/test/MainActivity.java
+++ app/src/main/java/com/hoversfw/test/MainActivity.java
@@ -8,14 +8,12 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.Context;
 import android.content.Intent;;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
