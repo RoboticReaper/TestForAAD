9acb7297c8943724e4c5e075b41f2783d837a56e
Your Name <liubaoren2006@gmail.com>
2020-04-29 16:38:36 -0400

    Removed unnecessary semicolon

--- app/src/main/java/com/hoversfw/test/MainActivity.java
+++ app/src/main/java/com/hoversfw/test/MainActivity.java
@@ -8,7 +8,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.Intent;;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
