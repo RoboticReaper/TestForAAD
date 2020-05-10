076ee987f40846d4e112bb3ee65d3a0c68a4509d
Your Name <liubaoren2006@gmail.com>
2020-05-10 00:07:31 -0400

    Added CustomView

--- app/src/main/java/com/hoversfw/test/CustomActivity.java
+++ app/src/main/java/com/hoversfw/test/CustomActivity.java
@@ -3,6 +3,10 @@ package com.hoversfw.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.hoversfw.test.views.CustomView;
 
 public class CustomActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class CustomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
+
+        final CustomView customView=findViewById(R.id.customView);
+        findViewById(R.id.swap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customView.swap();
+                Log.d("CustomActivity","button clicked");
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/hoversfw/test/views/CustomView.java
@@ -0,0 +1,66 @@
+package com.hoversfw.test.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView extends View {
+    private Rect rect;
+    private Paint paint;
+
+    public CustomView(Context context) {
+        super(context);
+
+        init(null);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    private void init(@Nullable AttributeSet set){
+        rect=new Rect();
+        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.GREEN);
+    }
+
+    public void swap(){
+        if(paint.getColor()==Color.GREEN){
+            paint.setColor(Color.RED);
+            Log.d("CustomView","Swapped to red");
+        }
+        else if(paint.getColor()==Color.RED){
+            paint.setColor(Color.GREEN);
+            Log.d("CustomView","Swapped to green");
+        }
+        postInvalidate();
+        Log.d("CustomView","Done");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        rect.left=50;
+        rect.top=50;
+        rect.bottom=rect.top+200;
+        rect.right=rect.left+200;
+        canvas.drawRect(rect,paint);
+    }
+}
